0ff9388f22b1e12e7a765a2fa117caedc62f1f60
pato <isabela.taborda@aluno.ifsp.edu.br>
2026-09-24 10:27:57 -0300

    Modiquei jogador

--- src/assets/content/arena.ts
+++ src/assets/content/arena.ts
@@ -3,8 +3,8 @@ import type { ArenaState } from "_types/arena";
 
 export const areaContent: ArenaState[] = [
   {
-    attribute: "aura",
-    deckA: "a-carlao",
+    attribute: "estilo",
+    deckA: "a-jorge",
     deckB: "b-pedro",
     playerAName: "Player-A",
     playerBName: "Player-B"
